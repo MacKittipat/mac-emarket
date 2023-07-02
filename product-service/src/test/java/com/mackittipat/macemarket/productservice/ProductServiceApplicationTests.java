254b161a74f13c7f0c5a7ba5020cfa019d5bf2f4
Mac <mac.kittipat@gmail.com>
2023-07-02 11:45:58 +0700

    product : Init project

---
+++ product-service/src/test/java/com/mackittipat/macemarket/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mackittipat.macemarket.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
